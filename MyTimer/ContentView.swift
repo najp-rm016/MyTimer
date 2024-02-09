c0ac6d4144ddb3586272f91a02d38f618a703c39
najp-rm016 <ryuichi.mochizuki@nippon-antenna.co.jp>
2024-02-08 20:51:48 -0800

    Initial Commit

---
+++ MyTimer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyTimer
+//
+//  Created by NSPC201admin on 2024/02/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by NSPC201admin on 2024/02/08.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
