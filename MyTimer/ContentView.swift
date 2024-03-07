b6d413df34766310db48698686de0ec4dd09b3e0
najp-rm016 <ryuichi.mochizuki@nippon-antenna.co.jp>
2024-03-07 15:54:19 +0900

    福嶋さんにご確認頂くためにコミット

--- MyTimer/ContentView.swift
+++ MyTimer/ContentView.swift
@@ -8,14 +8,94 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var timerHandler: Timer?
+    @State var count = 0
+    @AppStorage("timer_value") var timerValue = 10
+    @State var showAlert = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            ZStack{
+                Image("backgroundTimer")
+                    .resizable()
+                    .ignoresSafeArea()
+                    .aspectRatio(contentMode: .fill)
+                
+                VStack(spacing: 30){
+                    Text("残り\(timerValue - count)秒")
+                        .font(.largeTitle)
+                    
+                    HStack{
+                        Button(action: {
+                            startTimer()
+                        }){
+                            Text("スタート")
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(width: 140, height: 140)
+                                .background(Color("startColor"))
+                                .clipShape(Circle())
+                        }
+                        
+                        Button(action: {
+                            if let unwrapedTimerHandler = timerHandler{
+                                if unwrapedTimerHandler.isValid == true{
+                                    unwrapedTimerHandler.invalidate()
+                                }
+                            }
+                        }){
+                            Text("ストップ")
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(width: 140, height: 140)
+                                .background(Color("stopColor"))
+                                .clipShape(Circle())
+                        }
+                    }
+                    
+                }
+            }
+            .onAppear{
+                
+            }
+            .toolbar{
+                ToolbarItem(placement: .navigationBarTrailing){
+                    NavigationLink(destination: SettingView()){
+                        Text("秒数設定")
+                    }
+                }
+            }
+            .alert(isPresented: $showAlert){
+                Alert(title: Text("終了"),
+                      message: Text("タイマー終了時間です"),
+                      dismissButton: .default(Text("OK")))
+            }
         }
-        .padding()
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+    
+    func countDownTimer(){
+        count += 1
+        if timerValue - count <= 0{
+            timerHandler?.invalidate()
+            showAlert = true
+        }
+    }
+    
+    func startTimer(){
+        if let unwrapedTimerHandler = timerHandler{
+            if unwrapedTimerHandler.isValid == true{
+                return
+            }
+        }
+        
+        if timerValue - count <= 0{
+            count = 0
+        }
+        
+        timerHandler = Timer.scheduledTimer(withTimeInterval: 1, repeats: true){ _ in
+            countDownTimer()
+        }
     }
 }
 
---
+++ MyTimer/SwiftUIViews.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIViews.swift
+//  MyTimer
+//
+//  Created by NSPC201admin on 2024/02/08.
+//
+
+import SwiftUI
+
+struct SwiftUIViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIViews_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIViews()
+    }
+}
